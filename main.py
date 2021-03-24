b7f708eccfe900115a2cea5a6f2158e7d04a72b6
luis rodriguez <empresa.extendida@gmail.com>
2021-03-24 12:29:13 +0100

    Add Postgre instead of SQLite Add env variables

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, UserRegisterForm, UserLoginForm
 from functools import wraps
 import smtplib
+import os
 
 
 def send_registration_alert(new_user_email):
@@ -23,14 +24,14 @@ def send_registration_alert(new_user_email):
                              msg=message)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "any-secret-code-will-do")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
